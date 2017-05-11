5ccf30ab710dafc41795034ef5c01e16913e3bc1
alireza <alireza.eva.u23@gmail.com>
2017-05-11 22:14:11 +0000

    generates random shapes

--- gen.js
+++ gen.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const webshot = require('webshot'),
-jsdom = require('jsdom'), 
-d3 = require('d3'), 
-fs = require("fs");
+const
+    webshot = require('webshot'),
+    jsdom = require('jsdom'), 
+    d3 = require('d3'), 
+    fs = require('fs');
 
 // SGV Dimontions
 const
@@ -18,35 +19,67 @@ const options = {
     },
     siteType: 'html'
 }
+
 //load html file
-let htmlDoc = fs.readFileSync("html/index.html","utf8");
+let htmlDoc = fs.readFileSync("html/index.html", "utf8");
+
 // DOM
 let doc = jsdom.jsdom(htmlDoc);
 let svg = d3.select(doc.getElementById("test"))
     .append('svg')
     .attr('width', bg_w)
     .attr('height', bg_w)
-console.log(doc.body);
+
 // Target
-let c_r = 40;
-
-let target = svg.append('g')
-    .attr('transform', 'translate(' + c_r  + ',' + c_r + ')');
-
-target.append('circle')
-    .attr('r', c_r)
-    .attr('fill', 'green');
+const add_circle = function(svg, cr, cx, cy, cc, l, fs, tc){
+    let target = svg.append('g')
+        .attr('class', 'target')
+        .attr('transform', 'translate(' + (cx - cr / 2)  + ',' + (cy - cr / 2) + ')');
     
-target.append('text')
-    .text('A')
-    .attr('text-anchor', 'middle')
-    .attr('dominant-baseline', 'middle')
-    .attr('font-size', 40);
+    target.append('circle')
+        .attr('r', cr)
+        .attr('fill', cc);
+        
+    target.append('text')
+        .text(l)
+        .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'middle')
+        .attr('font-size', fs)
+        .attr('fill', tc);
+}
 
+const add_rand_circle = function(svg, bg_w, bg_h){
+    const colors = ['white', 'black', 'gray', 'red', 'blue', 'green', 'yellow', 'purple', 'brown', 'orange'];
+    const rand = m => Math.floor(Math.random() * (m + 1));
+    
+    const
+        cr = rand(Math.min(bg_w, bg_h) / 2),
+        cx = rand(bg_w),
+        cy = rand(bg_h),
+        cc = colors[rand(9)],
+        l = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(rand(25)),
+        tc = colors[rand(9)];
+        
+    add_circle(svg, cr, cx, cy, cc, l, cr, tc);
+}
 
 //uri of images to use for the bg make sure it's high rez
-const imagesUri = ["https://www.lawnstarter.com/blog/wp-content/uploads/2015/01/Yellow-st-augustine-lawn.jpg","http://eskipaper.com/images/grass-3.jpg"]
-for (var j = 0; j < imagesUri.length; j++){
-    doc.getElementById("img1").src = imagesUri[j];
-    webshot(doc.documentElement.outerHTML, `targets/hello_world${j}.png`, options, (err) => { console.log(err) });
+const imagesUri = [
+    "https://www.lawnstarter.com/blog/wp-content/uploads/2015/01/Yellow-st-augustine-lawn.jpg",
+    "http://eskipaper.com/images/grass-3.jpg"
+]
+
+for (let b = 0; b < imagesUri.length; b++){
+    doc.getElementById("img1").src = imagesUri[b]; // select background
+    
+    for(let i=0; i< 2; i++){
+        svg.selectAll('.target').data([]).exit().remove(); // remove previous target
+        add_rand_circle(svg, bg_w, bg_h) // add a random target
+        
+        webshot(
+            doc.documentElement.outerHTML,
+            `targets/circle_${b}_${i}.png`,
+            options, (err) => { if (err !== null){console.log(err)} }
+        );
+    }
 }
