2aec83db63c11bc9641c2949cafded2abd1880e5
alireza <alireza.eva.u23@gmail.com>
2017-05-12 06:01:47 +0000

    gen meta data file

--- gen.js
+++ gen.js
@@ -18,7 +18,7 @@ const options = {
         height: bg_h
     },
     siteType: 'html'
-}
+};
 
 //load html file
 let htmlDoc = fs.readFileSync("html/index.html", "utf8");
@@ -28,7 +28,7 @@ let doc = jsdom.jsdom(htmlDoc);
 let svg = d3.select(doc.getElementById("test"))
     .append('svg')
     .attr('width', bg_w)
-    .attr('height', bg_w)
+    .attr('height', bg_w);
 
 // Target
 const add_circle = function(svg, cr, cx, cy, cc, l, fs, tc){
@@ -46,40 +46,61 @@ const add_circle = function(svg, cr, cx, cy, cc, l, fs, tc){
         .attr('dominant-baseline', 'middle')
         .attr('font-size', fs)
         .attr('fill', tc);
-}
+};
 
 const add_rand_circle = function(svg, bg_w, bg_h){
     const colors = ['white', 'black', 'gray', 'red', 'blue', 'green', 'yellow', 'purple', 'brown', 'orange'];
-    const rand = m => Math.floor(Math.random() * (m + 1));
-    
+    const rand = (l, r) => Math.floor(Math.random() * (r - l + 1) + l);
+    const scale = Math.min(bg_w, bg_h);
     const
-        cr = rand(Math.min(bg_w, bg_h) / 2),
-        cx = rand(bg_w),
-        cy = rand(bg_h),
-        cc = colors[rand(9)],
-        l = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(rand(25)),
-        tc = colors[rand(9)];
+        cr = rand(scale / 10, scale / 2),
+        cx = rand(0, bg_w),
+        cy = rand(0, bg_h),
+        cc = colors[rand(0, 9)],
+        l = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(rand(0, 25)),
+        tc = colors[rand(0, 9)];
         
     add_circle(svg, cr, cx, cy, cc, l, cr, tc);
-}
+    
+    return {
+        shape: 'circle',
+        background_color: cc,
+        alphanumeric: l,
+        alphanumeric_color: tc,
+        x: cx - cr,
+        y: cy - cr,
+        w: cr, h: cr
+    };
+};
 
 //uri of images to use for the bg make sure it's high rez
 const imagesUri = [
     "https://www.lawnstarter.com/blog/wp-content/uploads/2015/01/Yellow-st-augustine-lawn.jpg",
     "http://eskipaper.com/images/grass-3.jpg"
-]
+];
+
+const targets = [];
+const num_samples_per_background = 10;
 
 for (let b = 0; b < imagesUri.length; b++){
     doc.getElementById("img1").src = imagesUri[b]; // select background
     
-    for(let i=0; i< 2; i++){
+    for(let i=0; i< num_samples_per_background; i++){
         svg.selectAll('.target').data([]).exit().remove(); // remove previous target
-        add_rand_circle(svg, bg_w, bg_h) // add a random target
+        let target = add_rand_circle(svg, bg_w, bg_h); // add a random target
+        let id = `t_${b}_${i}.png`;
+        target.id = id;
+        targets.push(target);
         
         webshot(
             doc.documentElement.outerHTML,
-            `targets/circle_${b}_${i}.png`,
-            options, (err) => { if (err !== null){console.log(err)} }
+            `targets/${id}`,
+            options, (err) => { if (err !== null){ console.log(err); } }
         );
     }
 }
+
+
+fs.writeFile('targets/disc.txt', JSON.stringify({targets, targets_count: targets.length}), 'utf8', (err) => {
+    if (err !== null){ console.log(err); }
+});
