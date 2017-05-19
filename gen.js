fe108281807a5d9ab625da013c9cb2b91bc5a7f9
alireza <alireza.eva.u23@gmail.com>
2017-05-19 09:46:50 +0000

    refactors

--- gen.js
+++ gen.js
@@ -30,89 +30,95 @@ let svg = d3.select(doc.getElementById("test"))
     .attr('width', bg_w)
     .attr('height', bg_w);
 
-// Target
-const add_text = function(target, text, font_size, color, size){
-    target.append('text')
-        .text(text).attr('x', size / 2).attr('y', size / 2)
-        .attr('text-anchor', 'middle')
-        .attr('dominant-baseline', 'middle')
-        .attr('font-family', 'Work Sans')
-        .attr('font-size', font_size)
-        .attr('fill', color);
-}
-const shift = function(svg, x, y) {
-    const target = svg.append('g')
-        .attr('class', 'target')
-        .attr('transform', 'translate(' + x  + ',' + y + ')');
-    return target;
-}
-const add_circle = function(target, size, color){
-    target.append('circle')
-        .attr('cx', size / 2)
-        .attr('cy', size / 2)
-        .attr('r', size / 2)
-        .attr('fill', color);
-    return 'circle';
-};
-const add_rect = function(target, size, color){
-    target.append('rect')
-        .attr('width', size)
-        .attr('height', size)
-        .attr('fill', color);
-    return 'rect';
-}
-const shape_fn = [add_circle, add_rect];
+// colors for shapes and text
 const colors = ['white', 'black', 'gray', 'red', 'blue', 'green', 'yellow', 'purple', 'brown', 'orange'];
-const rand = (l, r, e) => {
-    for(let i = 0; i< 100; i++) {
-        let n = Math.floor(Math.random() * (r - l + 1) + l);
-        if (n !== e) { return n; }
+
+// uri of images to use for the bg make sure it's high rez
+const imagesUri = [
+    "https://www.lawnstarter.com/blog/wp-content/uploads/2015/01/Yellow-st-augustine-lawn.jpg"
+];
+
+// Target
+const VIS = (function(){
+    const add_text = function(target, text, font_size, color, size){
+        target.append('text')
+            .text(text).attr('x', size / 2).attr('y', size / 2)
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'middle')
+            .attr('font-family', 'Work Sans')
+            .attr('font-size', font_size)
+            .attr('fill', color);
+    }  
+    const shift = function(svg, x, y) {
+        const target = svg.append('g')
+            .attr('class', 'target')
+            .attr('transform', 'translate(' + x  + ',' + y + ')');
+        return target;
     }
-    return l;
-}
-const rand_param = function(bg_w, bg_h, colors){
-    const bg_min_dim = Math.min(bg_w, bg_h);
-    const size = rand(bg_min_dim / 5, bg_min_dim / 4);
-    const c = rand(0, 9);
-    return {
-        size,
-        x: rand(0, bg_w - size),
-        y: rand(0, bg_h - size),
-        shape_color: colors[c],
-        text: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.charAt(rand(0, 35)),
-        text_size: Math.floor(size * 0.8),
-        text_color: colors[rand(0, 9, c)]
+    const add_circle = function(target, size, color){
+        target.append('circle')
+            .attr('cx', size / 2)
+            .attr('cy', size / 2)
+            .attr('r', size / 2)
+            .attr('fill', color);
+        return 'circle';
     };
-}
-const add_random_target = function(svg, bg_w, bg_h, colors, shape_fn){
-    let p = rand_param(bg_w, bg_h, colors);
-    let target = shift(svg, p.x, p.y);
-    let shape = shape_fn[Math.floor(Math.random() * shape_fn.length)](target, p.size, p.shape_color);
-    add_text(target, p.text, p.text_size, p.text_color, p.size);
-    p.shape = shape;
-    return p;
-}
+    const add_rect = function(target, size, color){
+        target.append('rect')
+            .attr('width', size)
+            .attr('height', size)
+            .attr('fill', color);
+        return 'rect';
+    }
+    const shape_fn = [add_circle, add_rect];
+    const rand = (l, r, e) => {
+        for(let i = 0; i< 100; i++) {
+            let n = Math.floor(Math.random() * (r - l + 1) + l);
+            if (n !== e) { return n; }
+        }
+        return l;
+    }
+    const rand_param = function(bg_w, bg_h, colors){
+        const bg_min_dim = Math.min(bg_w, bg_h);
+        const size = rand(bg_min_dim / 5, bg_min_dim / 4);
+        const c = rand(0, 9);
+        return {
+            size,
+            x: rand(0, bg_w - size),
+            y: rand(0, bg_h - size),
+            shape_color: colors[c],
+            text: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.charAt(rand(0, 35)),
+            text_size: Math.floor(size * 0.8),
+            text_color: colors[rand(0, 9, c)]
+        };
+    }
+    const add_random_target = function(svg, bg_w, bg_h, colors){
+        let p = rand_param(bg_w, bg_h, colors);
+        let target = shift(svg, p.x, p.y);
+        let shape = shape_fn[Math.floor(Math.random() * shape_fn.length)](target, p.size, p.shape_color);
+        add_text(target, p.text, p.text_size, p.text_color, p.size);
+        p.shape = shape;
+        return p;
+    }
+    
+    return {
+        add: add_random_target
+    };
+    
+})();
 
-
-const multiplex = function(i, n){
-    return Promise.all(
-        Array(n).fill(0).map((e, k)=> gen(`${i}_${k}`))
-    );
-};
-
-
-const gen = function(i) {
+const gen = function(id) {
     return new Promise((resolve,reject) => {
         doc.getElementById("img1").src = imagesUri[Math.floor(Math.random() * imagesUri.length)]; // select background    
         svg.selectAll('.target').data([]).exit().remove(); // remove previous target
-        let target = add_random_target(svg, bg_w, bg_h, colors, shape_fn); // add a random target
-        let id = `t_${i}.png`;
-        target.id = id;
+        let target = VIS.add(svg, bg_w, bg_h, colors); // add a random target
+        let name = `t_${id}.png`;
+        target.name = id;
         targets.push(target);
         
         webshot(
             doc.documentElement.outerHTML,
-            `targets/${id}`,
+            `targets/${name}`,
             options, (err) => { 
                 if (err !== null){
                     reject(`shit hit the fan ${err}`);
@@ -124,21 +130,17 @@ const gen = function(i) {
     });
 }
 
-
-
-//uri of images to use for the bg make sure it's high rez
-const imagesUri = [
-    "https://www.lawnstarter.com/blog/wp-content/uploads/2015/01/Yellow-st-augustine-lawn.jpg"
-];
-
+// RUN
 const targets = [];
-const num_of_samples_per_iter = 4;
-const num_iter = 3;
+const num_of_samples_per_iter = 10;
+const num_iter = 1000;
 
 let promise = Promise.resolve(); 
-for( let i = 0; i < num_iter; i++){
+for(let itr = 0; itr < num_iter; itr++){
     promise = promise.then((result) =>{
-        return multiplex(i, num_of_samples_per_iter);
+        return Promise.all(
+            Array(num_of_samples_per_iter).fill(0).map((e, ind)=> gen(`${itr}_${ind}`))
+        )
     });
 }
 
